edb73d7c17535742b8407b48345537d1797f5d7d
AJGit98 <122079133+AJGit98@users.noreply.github.com>
2023-02-16 10:45:56 +0530

    Create before_install.sh

---
+++ scripts/before_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Update Server before Work"
+apt update 
+echo "Server Updated"
+echo "starting the cloudwatch logs"
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-linux-adi-L2P6 -s
+sudo systemctl start amazon-cloudwatch-agent.service
+sudo systemctl enable amazon-cloudwatch-agent.service
+sudo systemctl status amazon-cloudwatch-agent.service
